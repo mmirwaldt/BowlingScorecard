d22f194f5a3e6736a94337b93c01252fdd15e79f
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-11 22:11:18 +0200

    TDD blue: turned BowlingGame variable into field

--- src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
+++ src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
+    private final BowlingGame bowlingGame = new BowlingGameByTDD();
+
     @Test
     @DisplayName("Given no ball is rolled, when no pins are hit, then the score is 0")
     void givenNoBallIsRolled_whenNoPinsAreHit_thenScoreIs0() {
-        BowlingGame bowlingGame = new BowlingGameByTDD();
         assertEquals(0, bowlingGame.score());
     }
 
     @Test
     @DisplayName("Given one ball is rolled, when no pins are hit, then the score is 0")
     void givenOneBallIsRolled_whenNoPinsAreHit_thenScoreIs0() {
-        BowlingGame bowlingGame = new BowlingGameByTDD();
         bowlingGame.roll(0);
         assertEquals(0, bowlingGame.score());
     }
