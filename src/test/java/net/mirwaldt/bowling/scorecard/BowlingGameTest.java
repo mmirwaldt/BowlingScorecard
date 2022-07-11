72d2fee6bac93775e8ee89216067a883cdf0b637
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-11 23:39:40 +0200

    TDD green: added test "Given two balls are rolled, when 10 pins are hit first and 3 second, ..." which passes

--- src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
+++ src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
@@ -100,5 +100,20 @@ public class BowlingGameTest {
             assertFalse(bowlingGame.isSpare());
             assertEquals(10 + 4, bowlingGame.score());
         }
+
+        @DisplayName("when 10 pins are hit first and 3 second, " +
+                "then it is one strike and the score is first 10 and second 10 + 3")
+        @Test
+        void whenOneStrikeFirstAnd3PinsSecond_thenItIsAStrikeAndScoreIsFirst10AndSecond13() {
+            bowlingGame.roll(10);
+            assertTrue(bowlingGame.isStrike());
+            assertFalse(bowlingGame.isSpare());
+            assertEquals(10, bowlingGame.score());
+
+            bowlingGame.roll(3);
+            assertFalse(bowlingGame.isStrike());
+            assertFalse(bowlingGame.isSpare());
+            assertEquals(10 + 3, bowlingGame.score());
+        }
     }
 }
