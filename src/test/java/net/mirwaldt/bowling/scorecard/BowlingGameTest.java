d648ea54957e6fa2b33c795eb09132a09faac7d1
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-11 22:07:06 +0200

    TDD blue: changed name from past tense to present tense

--- src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
+++ src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
@@ -1,11 +1,13 @@
 package net.mirwaldt.bowling.scorecard;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
     @Test
+    @DisplayName("Given no ball is rolled, when no pins are hit, then the score is 0")
     void givenNoBallIsRolled_whenNoPinsAreHit_thenScoreIs0() {
         BowlingGame bowlingGame = new BowlingGameByTDD();
         assertEquals(0, bowlingGame.score());
