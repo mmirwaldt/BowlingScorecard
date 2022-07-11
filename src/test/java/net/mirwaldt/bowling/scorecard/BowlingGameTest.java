d70907bb6849cb9be602dc8b9d5b30638ae006a6
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-11 22:02:43 +0200

    TDD red: added score() method so that first test fails with compiler error

--- src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
+++ src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
@@ -2,9 +2,12 @@ package net.mirwaldt.bowling.scorecard;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BowlingGameTest {
     @Test
     void givenNoBallWasRolled_whenNoPinsWereHit_thenScoreIs0() {
         BowlingGame bowlingGame = new BowlingGameByTDD();
+        assertEquals(0, bowlingGame.score());
     }
 }
