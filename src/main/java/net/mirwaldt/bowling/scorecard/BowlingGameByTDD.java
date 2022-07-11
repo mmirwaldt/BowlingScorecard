0ba3b6e499d9926d864f023dd4a7fe6bc9148f4d
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-11 23:35:25 +0200

    TDD green: test "Given two balls are rolled, when 10 pins are hit first and 4 second, ..." passes again

--- src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
+++ src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
@@ -25,6 +25,6 @@ public class BowlingGameByTDD implements BowlingGame {
 
     @Override
     public boolean isSpare() {
-        return rolled[0] + rolled[1] == 10;
+        return rolled[0] < 10 && rolled[0] + rolled[1] == 10;
     }
 }
