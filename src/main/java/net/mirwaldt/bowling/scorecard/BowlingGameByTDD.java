556e31c4e8f7fe31b4e2c883b2c39716c1aecad1
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-12 03:51:35 +0200

    TDD green: new tests "Given three balls are rolled, when one strikes is rolled first and second roll hits m pins and third roll hits n pins, ..." pass

--- src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
+++ src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
@@ -32,7 +32,7 @@ public class BowlingGameByTDD implements BowlingGame {
         if (pins < 0 || 10 < pins) {
             throw new IllegalArgumentException("The number of pins must be at least 0 and at most 10 but not " + pins);
         }
-        if (rolls % 2 == 1 && 10 < rolled[0] + pins) {
+        if (rolls % 2 == 1 && isStrike(index(frame(rolls))) && 10 < rolled[0] + pins) {
             throw new IllegalArgumentException("The sum of pins within a frame must be at most 10 but not "
                     + rolled[0] + " + " + pins + " == " + (rolled[0] + pins));
         }
