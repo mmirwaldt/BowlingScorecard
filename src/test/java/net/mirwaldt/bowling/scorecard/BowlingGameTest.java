139d0033d9af4ea6fe078614f4d2e4ba3e3285a8
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-12 03:26:03 +0200

    TDD green: added new test "Given three balls are rolled, when two strikes are rolled first and third roll hits 2 pins, ..."

--- src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
+++ src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest.java
@@ -208,5 +208,28 @@ public class BowlingGameTest {
             assertEquals(bowlingGame.score(1) + 10 + 6, bowlingGame.score(2));
             assertEquals(bowlingGame.score(2) + 6, bowlingGame.score());
         }
+
+        @DisplayName("when two strikes are rolled first and third roll hits 2 pins, " +
+                "then the score is first 10, second 30 and third 36")
+        @Test
+        void whenTwoStrikesAreRolledFirstAndThirdRollHitsNPins_thenScoreIsFirst10Second30AndThird30plusThreeTimes2() {
+            bowlingGame.roll(10);
+            assertTrue(bowlingGame.isStrike());
+            assertFalse(bowlingGame.isSpare());
+            assertEquals(10, bowlingGame.score());
+
+            bowlingGame.roll(10);
+            assertTrue(bowlingGame.isStrike());
+            assertFalse(bowlingGame.isSpare());
+            assertEquals(10 + 10, bowlingGame.score(1));
+            assertEquals(bowlingGame.score(1) + 10, bowlingGame.score());
+
+            bowlingGame.roll(2);
+            assertFalse(bowlingGame.isStrike());
+            assertFalse(bowlingGame.isSpare());
+            assertEquals(10 + 10 + 2, bowlingGame.score(1));
+            assertEquals(bowlingGame.score(1) + 10 + 2, bowlingGame.score(2));
+            assertEquals(bowlingGame.score(2) + 2, bowlingGame.score());
+        }
     }
 }
