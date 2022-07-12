8be09122fdf562a657eda131b6e89a46ec4bb8dd
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-12 02:14:07 +0200

    TDD blue: large refactoring to a different, new structure

--- src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
+++ src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
@@ -1,56 +1,93 @@
 package net.mirwaldt.bowling.scorecard;
 
+/**
+ * rolls | frame | index
+ *     0 |     0 |     0
+ *     1 |     1 |     0
+ *     2 |     1 |     0
+ *     3 |     2 |     2
+ *     4 |     2 |     2
+ *     5 |     3 |     4
+ *     6 |     3 |     4
+ *     7 |     4 |     6
+ *     8 |     4 |     6
+ *
+ *  frame(rolls) = (rolls + 1) / 2
+ *  index(frame) = (frame - 1) * 2
+ */
+
 public class BowlingGameByTDD implements BowlingGame {
-    private final int[] rolled = new int[3];
+    private final int[] rolled = new int[4];
 
     private int rolls;
 
     @Override
     public int score() {
-        if(rolls == 0) {
-            return score(0);
-        } else if((rolls == 1 || rolls == 2) && !isFirstRollStrike()) {
-            return score(1);
-        } else {
-            return score(2);
-        }
+        return score(frame(rolls));
     }
 
     @Override
     public void roll(int pins) {
-        if(pins < 0 || 10 < pins) {
+        if (pins < 0 || 10 < pins) {
             throw new IllegalArgumentException("The number of pins must be at least 0 and at most 10 but not " + pins);
         }
-        if(rolls == 1 && !isFirstRollStrike() && 10 < rolled[0] + pins) {
+        if (rolls == 1 && !isFirstRollStrike() && 10 < rolled[0] + pins) {
             throw new IllegalArgumentException("The sum of pins within a frame must be at most 10 but not "
-                    + rolled[0]  + " + " + pins + " == " + (rolled[0] + pins));
+                    + rolled[0] + " + " + pins + " == " + (rolled[0] + pins));
         }
-        rolled[rolls++] = pins;
+        rolled[rolls] = pins;
+
+        if (rolls % 2 == 0 && pins == 10) {
+            rolls += 2;
+        } else {
+            rolls++;
+        }
     }
 
     @Override
     public boolean isStrike() {
-        return (rolls == 1 && isFirstRollStrike()) || (rolled[0] != 0 && rolled[1] == 10);
+        int frame = frame(rolls);
+        int index = index(frame);
+        return 0 < rolls && rolled[index] == 10;
     }
 
     @Override
     public boolean isSpare() {
-        return !isFirstRollStrike() && areFirstTwoRollsSpare();
+        int frame = frame(rolls);
+        int index = index(frame);
+        return 1 < rolls && rolled[index] != 10 && rolled[index] + rolled[index + 1] == 10;
     }
 
     @Override
     public int score(int frame) {
-        if(frame == 0) {
-            return 0;
-        } else if(frame == 1) {
-            if(isFirstRollStrike() || areFirstTwoRollsSpare()) { // strike or spare
-                return rolled[0] + rolled[1] + rolled[2];
+        int maxRolls = frame * 2;
+        int score = 0;
+        for (int roll = 0; roll < maxRolls; ) {
+            int pins = rolled[roll];
+            if(pins == 10) {
+                score += 10;
+                if(roll + 2 < rolls) {
+                    score += rolled[roll + 2];
+                    if (rolled[roll + 2] == 10 && roll + 4 < rolls) {
+                        score += rolled[roll + 4];
+                    } else if(rolled[roll + 2] != 10 && roll + 3 < rolls) {
+                        score += rolled[roll + 3];
+                    }
+                }
+            } else if(roll + 1 < rolls && rolled[roll] + rolled[roll + 1] == 10) {
+                score += 10;
+                if(roll + 2 < rolls) {
+                    score += rolled[roll + 2];
+                }
             } else {
-                return rolled[0] + rolled[1];
+                score += pins;
+                if(roll + 1 < rolls) {
+                    score += rolled[roll + 1];
+                }
             }
-        } else {
-            return score(1) + ((isFirstRollStrike()) ? rolled[1] : rolled[2]);
+            roll += 2;
         }
+        return score;
     }
 
     private boolean areFirstTwoRollsSpare() {
@@ -59,5 +96,13 @@ public class BowlingGameByTDD implements BowlingGame {
 
     private boolean isFirstRollStrike() {
         return rolled[0] == 10;
+    }
+
+    private int frame(int roll) {
+        return (roll + 1) / 2;
+    }
+
+    private int index(int frame) {
+        return (frame - 1) * 2;
     }
 }
