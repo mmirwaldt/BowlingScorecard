fd848092c9ee1690609cdcba49e2e169bd092443
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-12 05:01:49 +0200

    TDD green: new test "Given four balls are rolled, when 10 pins are hit four times, ..." passes

--- src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
+++ src/main/java/net/mirwaldt/bowling/scorecard/BowlingGameByTDD.java
@@ -18,7 +18,7 @@ package net.mirwaldt.bowling.scorecard;
  */
 
 public class BowlingGameByTDD implements BowlingGame {
-    private final int[] rolled = new int[6];
+    private final int[] rolled = new int[8];
 
     private int rolls;
 
