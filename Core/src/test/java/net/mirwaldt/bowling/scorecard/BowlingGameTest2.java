42ed932ef55e820e43ae1fffa89f524b70b8ba96
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-22 22:53:54 +0200

    added second test class for old BowlingGameByTDD

---
+++ Core/src/test/java/net/mirwaldt/bowling/scorecard/BowlingGameTest2.java
@@ -0,0 +1,9 @@
+package net.mirwaldt.bowling.scorecard;
+
+@SuppressWarnings("NewClassNamingConvention")
+public class BowlingGameTest2 extends BowlingGameTest {
+
+    public BowlingGameTest2() {
+        game = new BowlingGameByTDD();
+    }
+}
