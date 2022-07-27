bf14e0f5a0bd42201c8b82fe269dbd05ccbbc92b
mmirwaldt <michael.mirwaldt@gmail.com>
2022-07-27 22:24:35 +0200

    TDD blue: rearranged method order in interface BowlingGame

--- Core/src/main/java/net/mirwaldt/bowling/scorecard/BowlingGame.java
+++ Core/src/main/java/net/mirwaldt/bowling/scorecard/BowlingGame.java
@@ -1,19 +1,12 @@
 package net.mirwaldt.bowling.scorecard;
 
 public interface BowlingGame {
-    int score();
-
     void roll(int pins);
-
     boolean isLastRollStrike();
-
     boolean isLastFrameSpare();
-
-    int score(int frame);
-
-    boolean isOver();
-
     int currentFrame();
-
     int currentRollInFrame();
+    int score(int frame);
+    int score();
+    boolean isOver();
 }
